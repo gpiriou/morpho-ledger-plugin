3bbda65faaf099b6ed5ef90368263f7edeb76ee9
gpiriou <piriou.glenn@gmail.com>
2022-08-25 17:02:38 +0200

    added claim test and renamed on_behalf var to user_address

--- tests/src/networks/ethereum/repay_dai.test.js
+++ tests/src/networks/ethereum/repay_dai.test.js
@@ -7,7 +7,7 @@ const testNetwork = "ethereum";
 const testLabel = "repay_dai"; // <= Name of the test
 const testDirSuffix = testLabel.toLowerCase().replace(/\s+/g, '_');
 
-// https://etherscan.io/tx/0x83d9edbf206976a776dcf3aeee0eeb6e69f29877dd065c1aea6d8587b0a33e77 // but on_behalf address is modified to match speculos address (5207948d16d4d78893692d369ae62888aa398f58 -> DAD77910DBDFDE764FC21FCD4E74D71BBACA6D8D).
+// https://etherscan.io/tx/0x83d9edbf206976a776dcf3aeee0eeb6e69f29877dd065c1aea6d8587b0a33e77 // but user_address address is modified to match speculos address (5207948d16d4d78893692d369ae62888aa398f58 -> DAD77910DBDFDE764FC21FCD4E74D71BBACA6D8D).
 const inputData = "0x1da649cf0000000000000000000000005d3a536e4d6dbd6114cc1ead35777bab948e3643000000000000000000000000DAD77910DBDFDE764FC21FCD4E74D71BBACA6D8D0000000000000000000000000000000000000000000074736e2a3a37bec74068";
 
 const models = [
--- tests/src/networks/ethereum/supply_token_warning.test.js
+++ tests/src/networks/ethereum/supply_token_warning.test.js
@@ -7,7 +7,7 @@ const testNetwork = "ethereum";
 const testLabel = "supply_token_warning"; // <= Name of the test
 const testDirSuffix = testLabel.toLowerCase().replace(/\s+/g, '_');
 
-// https://etherscan.io/tx/0x059909b8b2e1ed385cfdeb2812429abd93e4965af7ac57bd754adf3d4bc99e40 but on_behalf address is modified to match speculos address (dd2b3f1d3a4f08622a25a3f75284fc01ad0c5cca -> DAD77910DBDFDE764FC21FCD4E74D71BBACA6D8D). 
+// https://etherscan.io/tx/0x059909b8b2e1ed385cfdeb2812429abd93e4965af7ac57bd754adf3d4bc99e40 but user_address address is modified to match speculos address (dd2b3f1d3a4f08622a25a3f75284fc01ad0c5cca -> DAD77910DBDFDE764FC21FCD4E74D71BBACA6D8D). 
 // and cUSDC address modified to raise token warning (39aa3 -> 39aa4)
 const inputData = "0x0c0a769b00000000000000000000000039aa49c021dfbae8fac545936693ac917d5e7563000000000000000000000000DAD77910DBDFDE764FC21FCD4E74D71BBACA6D8D00000000000000000000000000000000000000000000000000000000004c4b40";
 
