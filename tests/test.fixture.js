20319e6c92e50faac9e6a956f24633bcd4a97860
Enzo Piriou <enzopiriou@gmail.com>
2022-08-27 12:59:19 +0200

    working nanoSP

--- tests/test.fixture.js
+++ tests/test.fixture.js
@@ -8,20 +8,22 @@ import ledgerService from "@ledgerhq/hw-app-eth/lib/services/ledger"
 const transactionUploadDelay = 60000;
 
 const sim_options_nano = {
-    ...DEFAULT_START_OPTIONS,
-    logging: true,
-    X11: true,
-    startDelay: 5000,
-    startText: 'is ready'
+  ...DEFAULT_START_OPTIONS,
+  logging: true,
+  X11: true,
+  startDelay: 5000,
+  startText: 'is ready'
 };
 
 const Resolve = require('path').resolve;
 
 const NANOS_ETH_PATH = Resolve('elfs/ethereum_nanos.elf');
 const NANOX_ETH_PATH = Resolve('elfs/ethereum_nanox.elf');
+const NANOSP_ETH_PATH = Resolve('elfs/ethereum_nanosp.elf');
 
 const NANOS_PLUGIN_PATH = Resolve('elfs/morpho_nanos.elf');
 const NANOX_PLUGIN_PATH = Resolve('elfs/morpho_nanox.elf');
+const NANOSP_PLUGIN_PATH = Resolve('elfs/morpho_nanosp.elf');
 
 const SPECULOS_ADDRESS = '0xDAD77910DBDFDE764FC21FCD4E74D71BBACA6D8D';
 const RANDOM_ADDRESS = '0xaaaabbbbccccddddeeeeffffgggghhhhiiiijjjj'
@@ -30,53 +32,54 @@ const NFT_EXPLORER_BASE_URL = "https://nft.api.live.ledger.com/v1/ethereum"
 const PLUGIN_BASE_URL = "https://cdn.live.ledger.com"
 
 const nano_models: DeviceModel[] = [
-    { name: 'nanos', letter: 'S', path: NANOS_PLUGIN_PATH, eth_path: NANOS_ETH_PATH },
-    { name: 'nanox', letter: 'X', path: NANOX_PLUGIN_PATH, eth_path: NANOX_ETH_PATH }
+  { name: 'nanos', letter: 'S', path: NANOS_PLUGIN_PATH, eth_path: NANOS_ETH_PATH },
+  { name: 'nanox', letter: 'X', path: NANOX_PLUGIN_PATH, eth_path: NANOX_ETH_PATH },
+  { name: 'nanosp', letter: 'SP', path: NANOSP_PLUGIN_PATH, eth_path: NANOSP_ETH_PATH }
 ];
 
 const resolutionConfig = {
-    externalPlugins: true,
-    nft: false,
-    erc20: false
+  externalPlugins: true,
+  nft: false,
+  erc20: false
 };
 
 let genericTx = {
-    nonce: Number(0),
-    gasLimit: Number(21000),
-    gasPrice: parseUnits('1', 'gwei'),
-    value: parseEther('1'),
-    chainId: 1,
-    to: RANDOM_ADDRESS,
-    data: null,
+  nonce: Number(0),
+  gasLimit: Number(21000),
+  gasPrice: parseUnits('1', 'gwei'),
+  value: parseEther('1'),
+  chainId: 1,
+  to: RANDOM_ADDRESS,
+  data: null,
 };
 
 const TIMEOUT = 1000000;
 
 function zemu(device, testNetwork, func) {
-    return async () => {
-        jest.setTimeout(TIMEOUT);
-        let elf_path;
-        let lib_elf;
-        elf_path = device.eth_path;
-        lib_elf = { 'Morpho': device.path };
+  return async () => {
+    jest.setTimeout(TIMEOUT);
+    let elf_path;
+    let lib_elf;
+    elf_path = device.eth_path;
+    lib_elf = { 'Morpho': device.path };
 
-        const sim = new Zemu(elf_path, lib_elf);
+    const sim = new Zemu(elf_path, lib_elf);
 
-        try {
-            await sim.start({ ...sim_options_nano, model: device.name });
-            const transport = sim.getTransport();
-            const eth = new Eth(transport);
-            eth.setLoadConfig({
-                // baseURL: null,
-                nftExplorerBaseURL: NFT_EXPLORER_BASE_URL,
-                pluginBaseURL: PLUGIN_BASE_URL,
-                extraPlugins: generate_plugin_config(testNetwork),
-            });
-            await func(sim, eth);
-        } finally {
-            await sim.close();
-        }
-    };
+    try {
+      await sim.start({ ...sim_options_nano, model: device.name });
+      const transport = sim.getTransport();
+      const eth = new Eth(transport);
+      eth.setLoadConfig({
+        // baseURL: null,
+        nftExplorerBaseURL: NFT_EXPLORER_BASE_URL,
+        pluginBaseURL: PLUGIN_BASE_URL,
+        extraPlugins: generate_plugin_config(testNetwork),
+      });
+      await func(sim, eth);
+    } finally {
+      await sim.close();
+    }
+  };
 }
 
 /**
@@ -89,29 +92,29 @@ function zemu(device, testNetwork, func) {
  * @param {string} unsignedTx unsignedTx to serialized
  */
 async function processTransaction(eth, sim, steps, label, testNetwork, unsignedTx) {
-    const loadConfig = {
-        nftExplorerBaseURL: NFT_EXPLORER_BASE_URL,
-        pluginBaseURL: PLUGIN_BASE_URL,
-        extraPlugins: generate_plugin_config(testNetwork),
-    };
+  const loadConfig = {
+    nftExplorerBaseURL: NFT_EXPLORER_BASE_URL,
+    pluginBaseURL: PLUGIN_BASE_URL,
+    extraPlugins: generate_plugin_config(testNetwork),
+  };
 
-    const serializedTx = ethers.utils.serializeTransaction(unsignedTx).slice(2);
+  const serializedTx = ethers.utils.serializeTransaction(unsignedTx).slice(2);
 
-    const resolution = await ledgerService.resolveTransaction(
-        serializedTx,
-        loadConfig,
-        resolutionConfig
-    );
+  const resolution = await ledgerService.resolveTransaction(
+    serializedTx,
+    loadConfig,
+    resolutionConfig
+  );
 
-    let tx = eth.signTransaction("44'/60'/0'/0/0", serializedTx, resolution);
+  let tx = eth.signTransaction("44'/60'/0'/0/0", serializedTx, resolution);
 
-    await sim.waitUntilScreenIsNot(
-        sim.getMainMenuSnapshot(),
-        transactionUploadDelay
-    );
-    await sim.navigateAndCompareSnapshots(".", label, [steps, 0]);
+  await sim.waitUntilScreenIsNot(
+    sim.getMainMenuSnapshot(),
+    transactionUploadDelay
+  );
+  await sim.navigateAndCompareSnapshots(".", label, [steps, 0]);
 
-    await tx;
+  await tx;
 }
 
 /**
@@ -125,52 +128,54 @@ async function processTransaction(eth, sim, steps, label, testNetwork, unsignedT
  * @param {string} testNetwork network name
  */
 function processTest(device, step, contractName, testLabel, testDirSuffix, unsignedTx, testNetwork = "ethereum") {
-    test(
-        "[" + device.letter + "]" + "[" + contractName + "] - " + testLabel,
-        zemu(device, testNetwork, async (sim, eth) => {
-            await processTransaction(
-                eth,
-                sim,
-                step,
-                testNetwork + "_" + device.name + "_" + testDirSuffix,
-                testNetwork,
-                unsignedTx
-            );
-        })
-    );
+  console.log('PENZO1 device', device)
+  test(
+    "[" + device.letter + "]" + "[" + contractName + "] - " + testLabel,
+    zemu(device, testNetwork, async (sim, eth) => {
+      console.log('PENZO device', device)
+      await processTransaction(
+        eth,
+        sim,
+        step,
+        testNetwork + "_" + device.name + "_" + testDirSuffix,
+        testNetwork,
+        unsignedTx
+      );
+    })
+  );
 }
 
 const supportedNetwork = {
-    'polygon': 137,
-    'ethereum': 1,
-    'bsc': 56,
-    'avalanche': 43114,
-    // 'celo': 42220,
-    // 'arbitrum': 42161,
-    // 'optimism': 10,
-    // 'fantom': 250,
+  'polygon': 137,
+  'ethereum': 1,
+  'bsc': 56,
+  'avalanche': 43114,
+  // 'celo': 42220,
+  // 'arbitrum': 42161,
+  // 'optimism': 10,
+  // 'fantom': 250,
 }
 
 function populateTransaction(contractAddr, inputData, networkName, value = "0.1") {
-    // Get the generic transaction template
-    let unsignedTx = genericTx;
-    // Adapt to the appropriate network
-    unsignedTx.chainId = supportedNetwork[networkName]
-    // Modify `to` to make it interact with the contract
-    unsignedTx.to = contractAddr;
-    // Modify the attached data
-    unsignedTx.data = inputData;
-    // Modify the number of ETH sent
-    unsignedTx.value = parseEther(value);
-    return unsignedTx;
+  // Get the generic transaction template
+  let unsignedTx = genericTx;
+  // Adapt to the appropriate network
+  unsignedTx.chainId = supportedNetwork[networkName]
+  // Modify `to` to make it interact with the contract
+  unsignedTx.to = contractAddr;
+  // Modify the attached data
+  unsignedTx.data = inputData;
+  // Modify the number of ETH sent
+  unsignedTx.value = parseEther(value);
+  return unsignedTx;
 }
 
 module.exports = {
-    zemu,
-    genericTx,
-    nano_models,
-    SPECULOS_ADDRESS,
-    RANDOM_ADDRESS,
-    processTest,
-    populateTransaction,
+  zemu,
+  genericTx,
+  nano_models,
+  SPECULOS_ADDRESS,
+  RANDOM_ADDRESS,
+  processTest,
+  populateTransaction,
 }
